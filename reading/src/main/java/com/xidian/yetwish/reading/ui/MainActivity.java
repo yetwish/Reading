d550f1bd7eb0268ae519b935e85a604abe11dfeb
Yetwish <yetwish@gmail.com>
2016-04-11 23:00:55 +0800

    ui of slideMenu .10

--- reading/src/main/java/com/xidian/yetwish/reading/ui/MainActivity.java
+++ reading/src/main/java/com/xidian/yetwish/reading/ui/MainActivity.java
@@ -2,16 +2,22 @@ package com.xidian.yetwish.reading.ui;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.xidian.yetwish.reading.R;
 
+/**
+ *
+ * main activity , use toolbar and drawerLayout to implement slideMenu
+ */
 public class MainActivity extends BaseActivity {
 
     private Toolbar mToolbar ;
-
+    private DrawerLayout mDrawerLayout;
 
     public static void startActivity(Context context){
         Intent intent = new Intent(context,MainActivity.class);
@@ -23,14 +29,38 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
+
     }
 
     private void initViews() {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
-        mToolbar.setLogo(R.mipmap.ic_menu_white_24dp);
+        mToolbar.setNavigationIcon(R.mipmap.ic_menu_white_24dp);
         mToolbar.setTitleTextColor(getResources().getColor(R.color.colorWhite));
         //TODO necessary?
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle mDrawerToogle = new ActionBarDrawerToggle(this,mDrawerLayout,mToolbar,
+                R.string.drawer_open,R.string.drawer_close){
+
+            //TODO
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+
+        mDrawerToogle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToogle);
+
     }
+
+
+
 }
